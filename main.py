5f8e4cf2af4c7c179894854be76fb07d2358b9c5
williamnyongesa <marakawilliamn@gmail.com>
2024-09-12 22:43:12 +0300

    finished

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import random
-target = random.randint(1,100)
+
+# Generate the target number once, outside the loop
+target = random.randint(1, 100)
 
 while True:
-    number = int(input("enter a number between 1 and 100 :"))
+    try:
+       number = int(input("Enter a number between 1 and 100: "))
+    except ValueError:
+        print("enter an integer value")
+        continue
 
     if number == target:
-        print( f"Congratulations!! you guessed right, The number is {target}")
-        break
-    if number > target:
-        print(f"number {number} is greater that target number")
-    elif number < target:
-        print(f"number {number} is less that target number")
+        print(f"Congratulations!! You guessed right! The number is {target}")
+        break  # Exit the loop when the guess is correct
+    elif number > target:
+        print(f"Your number {number} is greater than the target number.")
+    else:
+        print(f"Your number {number} is less than the target number.")
